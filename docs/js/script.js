9de66a2eb1479304a04d3cb0f30ab295bbe3f6cd
Yehor <egorkavin221100@gmail.com>
2020-06-12 21:44:05 +0300

    Added pagination and footer

--- docs/js/script.js
+++ docs/js/script.js
@@ -30,3 +30,50 @@ filters.forEach((filter) => {
 		});
 	}
 });
+
+const itemsFromAllPages = document.querySelectorAll('.product');
+let itemsPerPage = 4;
+let currentPage = 1;
+const paginationSection = document.querySelector('.pages-navigation');
+const pageNumberButtons = paginationSection.querySelector('.pages-navigation__page-numbers');
+
+function displayPage(pageNumber, itemsPerPage, allItems) {
+	const from = (pageNumber - 1) * itemsPerPage;
+	const arrOfNodes = Array.prototype.slice.call(allItems);
+	const pageItems = arrOfNodes.splice(from, itemsPerPage);
+	pageItems.forEach(item => item.removeAttribute('style'));
+	arrOfNodes.forEach(item => item.style.display = 'none');
+}
+
+function createPagination(paginationSection, itemsPerPage, allItems) {
+	const numberOfPages = Math.ceil(itemsFromAllPages.length / itemsPerPage);
+	const pageNumbersList = paginationSection.querySelector('.pages-navigation__list-of-pages');
+	for (let i = 0; i < numberOfPages; i++) {
+		const btn = createPageButton(i + 1, itemsPerPage, allItems);
+		pageNumbersList.appendChild(btn);
+	}
+}
+
+function createPageButton(pageNumber, itemsPerPage, allItems) {
+	const li = document.createElement('li');
+	const button = document.createElement('button');
+	li.classList.add('pages-navigation__page-wrapper');
+	button.classList.add('pages-navigation__page-button');
+	button.textContent = pageNumber;
+
+	if (pageNumber === currentPage) {
+		li.classList.add('pages-navigation__page-wrapper_active');
+	}
+	button.addEventListener('click', () => {
+		currentPage = pageNumber;
+		displayPage(pageNumber, itemsPerPage, allItems);
+		const prevPage = document.querySelector('.pages-navigation__page-wrapper.pages-navigation__page-wrapper_active');
+		prevPage.classList.remove('pages-navigation__page-wrapper_active');
+		li.classList.add('pages-navigation__page-wrapper_active');
+	});
+	li.appendChild(button);
+	return li;
+}
+
+displayPage(1, itemsPerPage, itemsFromAllPages);
+createPagination(paginationSection, itemsPerPage, itemsFromAllPages);
